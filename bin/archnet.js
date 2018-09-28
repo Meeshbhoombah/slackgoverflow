a165d706595a1eff7363c4e0d4eb2172786d79a1
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-09-27 20:59:51 -0700

    Removed logic

--- bin/archnet.js
+++ bin/archnet.js
@@ -1,17 +1,11 @@
 #!/usr/bin/env node
-
 "use strict";
 
 
-const Docker   = require('dockerode')
-,     daemon   = new Docker()
-,     program  = require('commander')
-,     chalk    = require('chalk');
+const program  = require('commander')
+,     :wq
+,     utils    = require('../src/utils.js');
 
-
-const log      = console.log;
-const notif    = function(str) {log(chalk.blueBright.bold(str));};
-const error    = function(str) {log(chalk.redBright("Error: " + str));};
 
 // $ -v, --version
 program
@@ -26,26 +20,14 @@ program
     .option('-g, --geth-nodes <# of nodes>', 
         'Set the number of `geth` nodes on the network (two by default).')
     .action(function(create) {
-        notif('Creating archnet...');
-        // create `archnet` network 
 
-        daemon.createNetwork({ Name: 'archnet', CheckDuplicate: true }, function(err, network) {
-            if (!err) {
-                log(network);
-            } else {
-                error(err.json.message)
-            }
-        });
-
-        // build containers
-        // expose ports and connect to network
-        // create coinbase for mining rewards
-        // start mining using `geth` command line
+        
     
-    });
-
+    };
+        
 
 program.parse(process.argv);
+
 
 // $ archnet
 if (!program.args.length) program.help();
