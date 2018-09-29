46de1beef4b440f6aa2f15d771875870ea4ec0d8
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-09-28 17:31:28 -0700

    Consolidated script into one file for now

--- bin/archnet.js
+++ bin/archnet.js
@@ -3,8 +3,14 @@
 
 
 const program  = require('commander')
-,     :wq
-,     utils    = require('../src/utils.js');
+,     Docker   = require('dockerrode')
+,     daemon   = new Docker()
+,     chalk    = require('chalk');
+
+
+const log      = console.log;
+,     vital    = function(str) {log(chalk.blueBright.bold(str));};
+,     shout    = function(str) {log(chalk.redBright("Error: " + str));};
 
 
 // $ -v, --version
@@ -13,17 +19,47 @@ program
     .version('0.0.1', '-v, --version');
 
 
-// $ archnet up -g
+// $ archnet boot
 program
-    .command('create')
+    .command('boot')
     .description('Build and deploy `archnet` in the default `dev` config.')
     .option('-g, --geth-nodes <# of nodes>', 
         'Set the number of `geth` nodes on the network (two by default).')
-    .action(function(create) {
-
+    .action(function(create) {  
         
-    
-    };
+        utils.vital('BOOTING ARCHNET...')
+        daemon.createNetwork({ Name: 'archnet', CheckDuplicate: true }, function(err, msg) {
+
+            // only if message in response
+            if (msg) {
+                utils.log(msg)
+                utils.vital('ARCHNET NETWORK BOOTED: WAITING FOR NODES...')
+            }
+
+            if (err && err.statusCode == 409)  {                  
+                utils.log('ARCHNET NETWORK EXISTS...');
+            } else {
+                shout(err.json.message);
+                process.exit(1);
+            }
+
+            // build containers
+            // expose ports and connect to network
+            // create coinbase for mining rewards
+            // start mining using `geth` command line
+        });
+    });
+
+
+// $ archnet stop
+program
+    .command('destroy')
+    .description('Stop the `archnet` network.')
+    .option('-c, --clean',
+        'Destroy and remove the network and nodes after stoping')
+    .action(function(destroy) {
+
+    });
         
 
 program.parse(process.argv);
