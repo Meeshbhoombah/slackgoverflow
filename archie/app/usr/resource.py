528ea7b755c6647bae507366e0e4ccd4848f4645
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-11-27 08:52:35 -0800

    Updated resource, removed Twilio boilerplate

--- archie/app/usr/resource.py
+++ archie/app/usr/resource.py
@@ -4,13 +4,12 @@
 import pprint
 from flask import request
 from flask_restful import Resource, reqparse
-from twilio.twiml.messaging_response import MessagingResponse
 from .model import User
 
 class User(Resource):
 
     def post(self):
-        """Recieve message from Twilio and act accordingly"""
+        """Create a new user"""
         pprint.pprint(request)
 
         return {'status': 'OK'}, 200
