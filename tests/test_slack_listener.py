47644b114674149cdc61b1c150ccef9de2f2c5c7
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-02-07 18:54:27 -0800

    Renamed/reorganized files and updated README

--- tests/test_slack_listener.py
+++ tests/test_slack_listener.py
@@ -1,26 +1,27 @@
 
 import unittest
 import time
-import datetime import datetime
+
+from app import create_app, db
+from app.models import Role, User, Question, Comment, Answer
 
 
-class TestListenSlack(unittest.TestCase)
+class TestListenSlack(unittest.TestCase):
 
     def setUp(self):
         self.app = create_app('testing')
-        self.app_context = self.app.app_context
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
         Role.insert_roles()
 
 
     def tearDown(self):
-        db.session_remove()
+        db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
 
     def test_verify_signature(self):
-        """ TODO: Generate a Slack signature """
         pass
 
