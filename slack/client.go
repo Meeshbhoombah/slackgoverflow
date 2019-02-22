ad18845781c7116255ea9a4b4cc41db8904eb0b6
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-02-22 15:43:34 -0800

    HOTFIX chan bug

--- slack/client.go
+++ slack/client.go
@@ -48,7 +48,7 @@ func attachSlackoverflow(sc *Client) error {
 	}
 
 	for _, channel := range channels {
-		if channel.Name == "devp2p" {
+		if channel.Name == "slackoverflow" {
 			sc.ChannelId = channel.ID
 		}
 		// TODO: error if channel not found
