62d5d63c15a0823b6d26efc917006490afc6e5aa
Asim Zaidi <asimzaidih@gmail.com>
2019-02-27 20:40:38 -0800

    Home Component, bg slack image, browser router, node-sass installed

---
+++ static/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import BackgroundImage from '../../Assets/Images/background-image.png'
+import '../../Styles/home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+      </div>
+    );
+  };
+};
+
+export default Home;
