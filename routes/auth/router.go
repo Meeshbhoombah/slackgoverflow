62cde08168a40bd70252b063fbc65b3e6e405a9f
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-03-01 11:06:42 -0800

    Deleted listen and routes

--- routes/auth/router.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import (
-	"github.com/labstack/echo"
-)
-
-func Routes(e *echo.Echo) {
-	e.GET("/authorize", AuthHandler)
-}
--- routes/static/router.go
+++
@@ -1,9 +0,0 @@
-package static
-
-import (
-	"github.com/labstack/echo"
-)
-
-func Routes(e *echo.Echo) {
-	e.Static("/", "static/build/index.html")
-}
