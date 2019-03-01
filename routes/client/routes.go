9bbce3a533d0d094d4936a94189d1455b96b9071
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-02-28 22:26:34 -0800

    Fixed static routes

--- routes/client/routes.go
+++
@@ -1,9 +0,0 @@
-package client
-
-import (
-	"github.com/labstack/echo"
-)
-
-func Routes(e *echo.Echo) {
-	e.Static("/", "static/public/index.html")
-}
--- routes/router.go
+++ routes/router.go
@@ -4,13 +4,13 @@ import (
 	"github.com/labstack/echo"
 
 	"github.com/archproj/slackoverflow/routes/auth"
-	"github.com/archproj/slackoverflow/routes/client"
 	"github.com/archproj/slackoverflow/routes/listen"
+	"github.com/archproj/slackoverflow/routes/static"
 )
 
 func Bind(e *echo.Echo) {
 	// render static files
-	client.Routes(e)
+	static.Routes(e)
 
 	// Slack OAuth 2.0 to integrate app
 	auth.Routes(e)
