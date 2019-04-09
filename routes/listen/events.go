401d0b5e9a8980b6f6d0f808f9f51bc498e1f603
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-04-09 14:27:18 -0700

    Added events handler - handing Challenges

--- routes/listen/events.go
+++ routes/listen/events.go
@@ -1,16 +1,45 @@
 package listen
 
-// Inconsistent API
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/nlopes/slack"
+	"github.com/labstack/echo"
+	s "github.com/nlopes/slack"
 	"github.com/nlopes/slack/slackevents"
+	log "github.com/sirupsen/logrus"
 )
 
 func EventHandler(c echo.Context) error {
-	eventsAPIEvent, e := slackevents.ParseEvent(json)
+	r := c.Request()
+	buf.ReadFrom(r.Body)
+	body := buf.String()
+	b := json.RawMessage(body)
+
+	evt, err := slackevents.ParseEvent(b)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	switch evt.Type {
+	case slackevents.URLVerification:
+		var r *slackevents.ChallengeResponse
+
+		err := json.Unmarshal([]byte(body), &r)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+
+		err = c.String(http.StatusOK, r.Challenge)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+	default:
+		return nil
+	}
 }
