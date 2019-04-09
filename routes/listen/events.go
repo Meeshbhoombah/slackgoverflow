ca3af07bb8db127aad065ba75fd716cf8542633a
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-04-08 21:38:46 -0700

    Added import statements for events

--- routes/listen/events.go
+++ routes/listen/events.go
@@ -1,5 +1,16 @@
 package listen
 
-func EventHandler(c echo.Context) error {
+// Inconsistent API
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
+)
+
+func EventHandler(c echo.Context) error {
+	eventsAPIEvent, e := slackevents.ParseEvent(json)
 }
