663c9e38139afe602a0c163353c005e4b84c63e8
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-04-09 15:05:26 -0700

    Working slackevents URLVerification

--- routes/listen/events.go
+++ routes/listen/events.go
@@ -3,30 +3,35 @@ package listen
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
-	s "github.com/nlopes/slack"
-	"github.com/nlopes/slack/slackevents"
+	s "github.com/nlopes/slack/slackevents"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/archproj/slackoverflow/config"
 )
 
 func EventHandler(c echo.Context) error {
 	r := c.Request()
+
+	buf := new(bytes.Buffer)
 	buf.ReadFrom(r.Body)
 	body := buf.String()
+
 	b := json.RawMessage(body)
 
-	evt, err := slackevents.ParseEvent(b)
+	cfg := c.Get("0").(*config.Variables)
+
+	evt, err := s.ParseEvent(b, s.OptionVerifyToken(&s.TokenComparator{VerificationToken: cfg.SlackVerToken}))
 	if err != nil {
 		log.Error(err)
 		return err
 	}
 
 	switch evt.Type {
-	case slackevents.URLVerification:
-		var r *slackevents.ChallengeResponse
+	case s.URLVerification:
+		var r *s.ChallengeResponse
 
 		err := json.Unmarshal([]byte(body), &r)
 		if err != nil {
@@ -40,6 +45,8 @@ func EventHandler(c echo.Context) error {
 			return err
 		}
 	default:
-		return nil
+		log.Info(evt.Type)
 	}
+
+	return nil
 }
